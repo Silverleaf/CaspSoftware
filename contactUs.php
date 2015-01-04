4798030b125df78312f6d576aecb7596717b74e8
Matthew Campbell <github-m@jelaryma.org>
2015-01-04 14:01:45 -0500

    Graphical Updates & code reformatting
    Updated the graphics and moved all css into separate stylesheets.

---
+++ contactUs.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link type="text/css" rel="stylesheet" href="content.css" />
+		<title>CaspSoftware - Contact Us</title>
+	</head>
+	<body>
+		<div class="dataHeader">
+			<div id="whatWeDo_wrapper">
+				<img src="whatwedo_nav@2x.png" alt="What We Do" id="whatWeDo">
+			</div>
+		</div>
+		
+		<div class="dataInfo">
+		</div>
+	</body>
+</html>
---
+++ whatWeDo.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link type="text/css" rel="stylesheet" href="content.css" />
+		<title>CaspSoftware - What We Do</title>
+	</head>
+	<body>
+		<div class="dataHeader">
+			<div id="contact_wrapper">
+				<img src="contact_nav@2x.png" alt="Contact Us" id="contactUs">
+			</div>
+		</div>
+		<div class="dataInfo">
+		</div>
+	</body>
+</html>
