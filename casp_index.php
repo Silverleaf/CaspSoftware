67d0d704c23e5a974792bd6af34d7f1194cd4304
Matthew Campbell <github-m@jelaryma.org>
2015-01-04 14:03:33 -0500

    main page

---
+++ casp_index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<link type="text/css" rel="stylesheet" href="main.css" />
+		<title>CaspSoftware</title>
+	</head>
+	<body>
+	
+		<div id="logo_wrapper">
+			<img src="casp_header@2x.png" id="header_label" alt="Header" width="574px" height="76px">
+		</div>
+
+		<div id="whatWeDo_wrapper">
+			<img src="whatwedo_nav@2x.png" alt="What We Do" id="whatWeDo">
+		</div>
+
+		<div id="contact_wrapper">
+			<img src="contact_nav@2x.png" alt="Contact Us" id="contactUs">
+		</div>
+	</body>
+</html>
